c5825976bcc89420ffc303dbe1850b655456643d
Ahmed Mohamed Yousef <ahmed.youssef@appetito.com>
2023-11-24 10:42:36 +0200

    Creating colors manager

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp._internal();
+
+  static final MyApp _instance =
+      MyApp._internal(); // singleton or single instance
+
+  factory MyApp() => _instance;
 
   @override
   Widget build(BuildContext context) {
